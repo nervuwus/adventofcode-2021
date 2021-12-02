211c83e51c4b96cb28700f2b60024445000d7c39
Nervous Nightingale <nightingale@nervous.localdomain>
2021-12-02 23:41:45 +0100

    complétition jour 2

---
+++ Day_2/puzzle1.lua
@@ -0,0 +1,50 @@
+local open = io.open
+
+local values = {}
+local direction = {}
+
+local calculate_direction = {}
+
+calculate_direction["forward"] = 0
+calculate_direction["depth"] = 0
+
+local function read_file(path)
+    local file = open(path, "rb")
+    if not file then
+        return nil
+    end
+    local content = file:read("*a")
+    file:close()
+    return content
+end
+
+local function calculation( dir, distance )
+        if dir == "forward" then
+            calculate_direction["forward"] = calculate_direction["forward"] + distance
+        elseif dir == "down" then
+            calculate_direction["depth"] = calculate_direction["depth"] + distance
+        elseif dir == "up" then
+            calculate_direction["depth"] = calculate_direction["depth"] - distance
+        end
+    end
+
+
+local filecontent = read_file("input.txt")
+
+for token in string.gmatch(filecontent, "[^%s]+") do
+    if tonumber(token) ~= nil then
+        table.insert(values, tonumber(token))
+    elseif tonumber(token) == nil then
+        table.insert(direction, token)
+    end
+end
+
+for i = 1, #values do
+    print("direction: ", direction[i])
+    print("distance: ", values[i])
+    calculation(direction[i], values[i])
+end
+
+print("forward value: ", calculate_direction["forward"])
+print("depth value: ", calculate_direction["depth"])
+print("total: ", calculate_direction["forward"] * calculate_direction["depth"])
